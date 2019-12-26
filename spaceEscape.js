d4aa484278bd8ae2cf7eb1d52e7ab2197f5ea19e
Raphael <raphael.ferreira@hetic.net>
2019-12-26 16:24:52 +0100

    meteors dropping

--- spaceEscape.js
+++ spaceEscape.js
@@ -11,9 +11,35 @@ bg.src = "images/bg.png";
 meteor.src = "images/meteor.png";
 ship.src = "images/spaceship.png";
 
+//Variables
+let gravity = 2;
+let shipX = 124;
+let distance = 100;
+
+let meteors = [];
+
+meteors[0] = {
+  x: 0,
+  y: 0
+};
+
+//Draw
+
 function draw() {
   context.drawImage(bg, 0, 0);
-  context.drawImage(ship, 124, 450, 40, 40);
+  for (let i = 0; i < meteors.length; i++) {
+    context.drawImage(meteor, meteors[i].x, meteors[i].y);
+    meteors[i].y += gravity;
+
+    //New Meteor
+    if (meteors[i].y == distance) {
+      meteors.push({
+        x: Math.floor(Math.random() * canvas.width),
+        y: 0
+      });
+    }
+  }
+  context.drawImage(ship, shipX, 450, 40, 40);
 
   requestAnimationFrame(draw);
 }
