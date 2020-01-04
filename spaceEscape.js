4247b8bcdd6293d19ffd3d638525c23837bfd928
Raphael <raphael.ferreira@hetic.net>
2020-01-04 12:44:45 +0100

    end the hitbox problem and add difficuties

--- spaceEscape.js
+++ spaceEscape.js
@@ -12,11 +12,12 @@ meteor.src = "images/meteor.png";
 monster.src = "images/monster.png";
 
 //Variables
+
 let gravity = 2;
 let monsterX = 106;
 let distance = 120;
 let score = 0;
-let life = 3;
+let life = 0;
 let direction = 36;
 
 let meteors = [];
@@ -48,22 +49,41 @@ function onKeyDown(event) {
 //Draw
 
 function draw() {
+  //Background
   context.drawImage(bg, 0, 0);
+  //Losing
+  for (let index = 0; index < meteors.length; index++) {
+    if (life === 3) {
+      location.reload();
+    }
+  }
+
+  //Difficulty
+  for (let j = 0; j < meteors.length; j++) {
+    if (score === 25) {
+      gravity = 3;
+    } else if (score === 50) {
+      gravity = 4;
+    } else if (score === 100) {
+      gravity = 5;
+    }
+  }
+
+  //Hitbox
   for (let i = 0; i < meteors.length; i++) {
-    //New Meteor
     if (
       meteors[i].y + 36 >= 430 &&
       ((meteors[i].x >= monsterX && meteors[i].x <= monsterX + 75) ||
         (meteors[i].x + 36 >= monsterX && meteors[i].x + 36 <= monsterX + 75))
     ) {
-      /* score++;
-      console.log(score); */
+      meteors.shift(i, 1);
+      score++;
     } else if (meteors[i].y >= canvas.height) {
       meteors.shift(i, 1);
-      console.log(meteors.length);
+      life++;
     }
   }
-
+  //Meteors
   for (let i = 0; i < meteors.length; i++) {
     context.drawImage(meteor, meteors[i].x, meteors[i].y, 36, 36);
     meteors[i].y += gravity;
@@ -75,10 +95,13 @@ function draw() {
         y: 0
       });
     }
-    //hitbox
   }
   window.addEventListener("keydown", onKeyDown);
   context.drawImage(monster, monsterX, 430, 75, 75);
+
+  context.fillStyle = "#000";
+  context.font = "20px Verdana";
+  context.fillText("Score :" + score, 10, canvas.height - 10);
 
   requestAnimationFrame(draw);
 }
